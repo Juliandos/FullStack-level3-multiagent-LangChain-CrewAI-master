bf243d9e9f99c299d9563513b2effc023b5b254a
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-11-01 15:39:35 -0500

    feat: add base UI components for technology and business area workflow

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+
+import {useState} from "react";
+import InputSection from "@/components/InputSection";
+
 export default function Home() {
+  const [technologies, setTechnologies] = useState([]);
+  const [businessareas, setBusinessareas] = useState([]); 
+
   return (
     <div className="bg-white min-h-screen text-black">
-    Home page
+      <div className="flex flex-col">
+        {/* Input sections in one row */}
+        <div className="flex w-full">
+          <div className="w-1/2 p-4">
+            <InputSection
+              title="Technologies"
+              placeholder="Example: Generative AI"
+              data={technologies}
+              setData={setTechnologies}
+            />
+          </div>
+          <div className="w-1/2 p-4">
+            <InputSection
+              title="Business Areas"
+              placeholder="Example: Customer Service"
+              data={businessareas}
+              setData={setBusinessareas}
+            />
+          </div>
+        </div>
+
+        {/* Output section and event log in a single column below */}
+        <div className="flex flex-col w-full p-4">
+          <div className="flex justify-between items-center mb-4">
+          <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded text-sm"
+            >
+              Start
+            </button>
+          {/* TODO: FINAL OUTPUT */}
+          {/* TODO: EVENT LOG */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
